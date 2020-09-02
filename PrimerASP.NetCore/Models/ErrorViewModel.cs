96ad93e0bb0365ca4d9ef6817dd004d2e173896d
Islas Tomas <tomas_islas@hotmail.com>
2020-09-02 18:19:45 -0300

    Primer commit Curso ASP.Net

---
+++ PrimerASP.NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PrimerASP.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
